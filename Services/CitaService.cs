678ec9dbe87784220b2d956b677bad4080a6630b
Nikol M <nikol.mosquera@upb.edu.co>
2026-03-16 16:17:28 -0500

    Modelos Cliente y Cita + Services

---
+++ Services/CitaService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public class CitaService
+{
+    private List<Cita> citas = new List<Cita>();
+
+    public void AgregarCita(Cita cita)
+    {
+        citas.Add(cita);
+    }
+
+    public List<Cita> ObtenerCitas()
+    {
+        return citas;
+    }
+}
