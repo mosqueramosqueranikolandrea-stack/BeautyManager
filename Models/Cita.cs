9ba6717808c9bcf227ba799f53909cdf8aae0ba7
Nikol M <nikol.mosquera@upb.edu.co>
2026-03-16 16:11:35 -0500

    Creación modelo Cita

---
+++ Models/Cita.cs
@@ -0,0 +1,10 @@
+﻿public class Cita
+{
+    public int Id { get; set; }
+
+    public string Cliente { get; set; }
+
+    public string Servicio { get; set; }
+
+    public DateTime Fecha { get; set; }
+}
