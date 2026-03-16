1dddec3d29114437d628945d55cd16d5e91986c9
Nikol M <nikol.mosquera@upb.edu.co>
2026-03-16 15:37:18 -0500

    Creación modelo Cliente y ClienteService

---
+++ Services/ClienteService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public class ClienteService
+{
+    private List<Cliente> clientes = new List<Cliente>();
+
+    public void AgregarCliente(Cliente cliente)
+    {
+        clientes.Add(cliente);
+    }
+
+    public List<Cliente> ObtenerClientes()
+    {
+        return clientes;
+    }
+}
