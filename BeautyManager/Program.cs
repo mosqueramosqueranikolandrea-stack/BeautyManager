e8d12b00cfb1683330f818798995f042e00069d6
Nikol M <nikol.mosquera@upb.edu.co>
2026-03-16 15:26:45 -0500

    Estructura inicial del proyecto BeautyManager

---
+++ BeautyManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BeautyManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
